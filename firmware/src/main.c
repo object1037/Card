6d6ba4d2574edc52ec1e289a325d6db5182ef884
object1037 <contact@object1037.dev>
2025-06-30 18:58:55 +0900

    interrupt test

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -1,21 +1,40 @@
 #include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/i2c.h>
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(app, LOG_LEVEL_DBG);
 
-#define SLEEP_TIME_MS 500
+#define SLEEP_TIME_MS 1000
 
+#define INT0_NODE DT_ALIAS(int0)
 #define I2C0_NODE DT_ALIAS(trackpad0)
 
-#define TOUCH_STATE 0x10
+#define TOUCH_STATE_ADDR 0x10
 
+static const struct gpio_dt_spec int0 = GPIO_DT_SPEC_GET(INT0_NODE, gpios);
 static const struct i2c_dt_spec trackpad = I2C_DT_SPEC_GET(I2C0_NODE);
 
+void int0_callback(const struct device *dev, struct gpio_callback *cb, uint32_t pins) {
+  LOG_INF("Interrupt triggered on pin %d", int0.pin);
+}
+
+static struct gpio_callback int0_cb_data;
+
 int main(void) {
-	int ret;
+  int ret;
+
+  ret = gpio_pin_interrupt_configure_dt(&int0, GPIO_INT_EDGE_TO_ACTIVE);
+  if (ret < 0) {
+    LOG_ERR("Failed to configure interrupt");
+    return 0;
+  }
+
+  gpio_init_callback(&int0_cb_data, int0_callback, BIT(int0.pin));
+  gpio_add_callback(int0.port, &int0_cb_data);
 
   if (!device_is_ready(trackpad.bus)) {
+    LOG_ERR("Trackpad not ready");
     return 0;
   }
   LOG_INF("Starting application...");
@@ -23,8 +42,7 @@ int main(void) {
   while (1) {
     uint8_t touch_data[5] = {0};
 
-    ret = i2c_burst_read_dt(&trackpad, TOUCH_STATE, touch_data, sizeof(touch_data));
-
+    ret = i2c_burst_read_dt(&trackpad, TOUCH_STATE_ADDR, touch_data, sizeof(touch_data));
     if (ret < 0) {
       LOG_ERR("I2C communication failed: %d", ret);
       return 0;
@@ -34,9 +52,6 @@ int main(void) {
       LOG_WRN("Lrg detected");
     }
     if (touch_data[0] & 0x02) {
-      if (ret < 0) {
-        return 0;
-      }
       switch (touch_data[4]) {
         case 0x00:
           LOG_INF("Gesture: None");
@@ -81,6 +96,7 @@ int main(void) {
     }
 
     LOG_INF("0x%02X (%d, %d)", touch_data[0], ((touch_data[1] << 4) | (touch_data[3] >> 4)), ((touch_data[2] << 4) | (touch_data[3] & 0x0F)));
+
     k_msleep(SLEEP_TIME_MS);
   }
   return 0;
